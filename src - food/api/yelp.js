6b2962538ba0fcf595aaa772bfa5c6b76d8fe83b
Talitha van Lith <43021762+talithavanlith@users.noreply.github.com>
2020-06-23 13:42:01 +1200

    Delete yelp.js

--- src - food/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer 4aQzrcPjnCpAqmcMhKNqyPNLXTKR6Dsjshq-4CQUmyyi4DWJZ2AL_zOohcRpWAvOnm6tvm0sPbDbdA0H6N2o7IwwPBVqPf7nXWvcQ8kfyM3NtsVKM42wuRhdje97XnYx'
-    }
-});
